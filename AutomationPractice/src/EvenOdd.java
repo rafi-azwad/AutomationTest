a3574c99948b64103546bc6054005f4bece8ab29
rafi-azwad <rafi.azwad@gmail.com>
2023-01-13 22:55:50 +0600

    check even odd number

---
+++ AutomationPractice/src/EvenOdd.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter any positive number ");
+        int num = input.nextInt();
+
+        if(num%2==0)
+            System.out.println(num + " is even number");
+        else
+            System.out.println(num + " is odd number");
+    }
+}
